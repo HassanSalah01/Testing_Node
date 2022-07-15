c1fd871cd4b83f76db532cee4ba3323ee5b4c104
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-07-15 22:51:20 +0200

    ADDED_.gitignore_And_ADDED_App_js

---
+++ app.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const {readFileSync } = require('fs');
+const Person = require('./Person');
+const pa = path.resolve(__dirname,'testing','test','text.txt');
+const first = readFileSync(pa,"utf8");
+let newFirst = "";
+for (let i of first){
+    if(i!="\n" & i!='\r'){
+        newFirst+=i
+    }else{
+        newFirst+=" ";
+    }
+}
+let arr2 = []
+const arr = newFirst.split(" ");
+for (let i = 0 ; i < arr.length+1;i+=5){
+    arr2.push(new Person(arr[i],arr[i+1],arr[i+2],arr[i+3]));
+}
+for (let i of arr2){
+    i.getGpa();
+    console.log("____________________________")
+}
+
+
+// const p = path.join("/cont","ser","foo")
+// console.log(p)
+// const p1 = path.resolve(__dirname);
+// console.log(p1)
+
+
+// os module
+// const {uptime} = require('os');
+// console.log(os.arch())
+// console.log(os.cpus())
+// console.log(uptime())
+
+
+
+
+// const { ser }= require('./1');
+
+
+
+
+
