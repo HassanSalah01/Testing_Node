521fb587522f0d5c6a21466577de64255ef64574
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-07-23 22:46:55 +0200

    Added_apiTest.js

---
+++ apiTest.js
@@ -0,0 +1,25 @@
+const http =  require("http");
+const EventEmitter = require("events");
+const {foo} = require("./events")
+const port = 3000;
+const arr= [{name:"ahmed",age:20},{name:"ahmed",age:20},{name:"ahmed",age:20}]
+
+class MyEmitter extends EventEmitter{};
+const myEmitter = new MyEmitter();
+
+
+myEmitter.on("log",(x)=>{
+    foo(x)
+})
+const server = http.createServer((req,res)=>{
+    console.log("welcome")
+    if(req.url==="/"){
+        myEmitter.emit("log",req.url)
+        res.end(JSON.stringify(arr))
+    }else if(req.url==="/post1"){
+        myEmitter.emit("log",req.url)
+        res.end(JSON.stringify(arr[0]))
+    }
+})
+
+server.listen(port,()=>{console.log("server is running")});
