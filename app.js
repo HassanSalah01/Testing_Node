edd328672fa6bbaf938cc9831fb0118210340c27
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-23 22:59:27 +0200

    Updated_app.js

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const path = require('path');
-const {readFileSync } = require('fs');
-const Person = require('./Person');
-const pa = path.resolve(__dirname,'testing','test','text.txt');
-const first = readFileSync(pa,"utf8");
-let newFirst = "";
-for (let i of first){
-    if(i!="\n" & i!='\r'){
-        newFirst+=i
-    }else{
-        newFirst+=" ";
-    }
+const Fdata = require('./FData')
+for (let i = 0 ; i < 100000; i++){
+    new Fdata();
 }
-let arr2 = []
-const arr = newFirst.split(" ");
-for (let i = 0 ; i < arr.length+1;i+=5){
-    arr2.push(new Person(arr[i],arr[i+1],arr[i+2],arr[i+3]));
-}
-for (let i of arr2){
-    i.getGpa();
-    console.log("____________________________")
-}
+
+
+
+
+
+
+
+
+// const path = require('path');
+// const {readFileSync } = require('fs');
+// const Person = require('./Person');
+// const pa = path.resolve(__dirname,'testing','test','text.txt');
+// const first = readFileSync(pa,"utf8");
+// let newFirst = "";
+// for (let i of first){
+//     if(i!="\n" & i!='\r'){
+//         newFirst+=i
+//     }else{
+//         newFirst+=" ";
+//     }
+// }
+// let arr2 = []
+// const arr = newFirst.split(" ");
+// for (let i = 0 ; i < arr.length+1;i+=5){
+//     arr2.push(new Person(arr[i],arr[i+1],arr[i+2],arr[i+3]));
+// }
+// for (let i of arr2){
+//     i.printing();
+//     console.log("____________________________")
+// }
 
 
 // const p = path.join("/cont","ser","foo")
