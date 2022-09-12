472c26e86ad82823ba8e1d0e96fd1f59fafca4a1
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-09-12 23:49:49 +0200

    Added_Jsoop_app1_app2

---
+++ jsoop/ap1.js
@@ -0,0 +1,48 @@
+let arr1 = [1 ,-10 ,-20, 15 ,100, -30]
+
+
+let newArr = arr1.map((element)=>{
+    return -element;
+})
+console.log(arr1);
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+// let arr2 = "elZERo";
+
+// const arr = arr2.split("");
+// // console.log(arr)
+// const newArr = arr.map((element)=>{
+//     if(element===element.toUpperCase()){
+//         return element.toLowerCase();
+//     }else{
+//         return element.toUpperCase();
+//     }
+// })
+
+// console.log(newArr.join('#'));
+
+
+
+
+
+
+
+// let arr = [2, 3, 5, 7]
+
+//  const add=arr.map(function(element, index, array){
+//     console.log(element);
+//     console.log(index);
+//     console.log(array);
+//     console.log(this)
+//     return element;
+// }, element);
+
+// console.log(add);
