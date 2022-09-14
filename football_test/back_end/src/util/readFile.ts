b3cbaccdcf75ceae25df645afe2d437743917275
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-09-14 21:42:44 +0200

    Updated_the_readFile

--- football_test/back_end/src/util/readFile.ts
+++ football_test/back_end/src/util/readFile.ts
@@ -1,10 +1,10 @@
-// import { promises as fs } from "fs";
+import { promises as fs } from "fs";
 
-// export const read = async (path: string): Promise<void> => {
-//     try {
-//         const line = await fs.readFile(path, "utf8");
-//         console.log(line);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
+export const read = async (path: string): Promise<void> => {
+    try {
+        const line = await fs.readFile(path, "utf8");
+        console.log(line);
+    } catch (error) {
+        console.log(error);
+    }
+};
