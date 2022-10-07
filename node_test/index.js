094d77da24e7fa8c2650882aeac45064d7b1895f
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-07 20:58:12 +0200

    Added_More_Testing_Node

---
+++ node_test/index.js
@@ -0,0 +1,16 @@
+setImmediate(()=>{
+    console.log("1")
+    setTimeout(()=>{
+        setImmediate(()=>{
+            console.log("2")
+        })
+        console.log("3")
+    },1000)
+})
+process.nextTick(()=>{
+    console.log("4")
+    setTimeout(()=>{
+        console.log("5")
+    },1000)
+})
+console.log("welcome")
