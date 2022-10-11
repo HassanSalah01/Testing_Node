04d7f935a1e27b04897584039eceb0999e44880b
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-11 21:02:48 +0200

    Updated_Api_Test

--- apiTest.js
+++ apiTest.js
@@ -1,25 +1,30 @@
-const http =  require("http");
+const http = require("http");
 const EventEmitter = require("events");
-const {foo} = require("./events")
+const { foo } = require("./events");
 const port = 3000;
-const arr= [{name:"ahmed",age:20},{name:"ahmed",age:20},{name:"ahmed",age:20}]
+const arr = [
+    { name: "ahmed", age: 20 },
+    { name: "ahmed", age: 20 },
+    { name: "ahmed", age: 20 },
+];
 
-class MyEmitter extends EventEmitter{};
+class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 
-
-myEmitter.on("log",(x)=>{
-    foo(x)
-})
-const server = http.createServer((req,res)=>{
-    console.log("welcome")
-    if(req.url==="/"){
-        myEmitter.emit("log",req.url)
-        res.end(JSON.stringify(arr))
-    }else if(req.url==="/post1"){
-        myEmitter.emit("log",req.url)
-        res.end(JSON.stringify(arr[0]))
+myEmitter.on("log", (x) => {
+    foo(x);
+});
+const server = http.createServer((req, res) => {
+    console.log("welcome");
+    if (req.url === "/") {
+        myEmitter.emit("log", req.url);
+        res.end(JSON.stringify(arr));
+    } else if (req.url === "/post1") {
+        myEmitter.emit("log", req.url);
+        res.end(JSON.stringify(arr[0]));
     }
-})
+});
 
-server.listen(port,()=>{console.log("server is running")});
+server.listen(port, () => {
+    console.log("server is running");
+});
