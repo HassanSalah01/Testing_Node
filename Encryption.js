b60d9f0832ef2687eaebe83c48df0a5fad2095ab
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-01 23:44:26 +0200

    Updated_encryption

--- Encryption.js
+++ Encryption.js
@@ -12,6 +12,25 @@ function rand() {
     return randomNumber;
 }
 
+function allArray() {
+    for (let x = 0; x < user.length; x++) {
+        userAr.push(user[x]);
+        ne.push(rand());
+    }
+    for (let x = 0; x < inc.length; x++) {
+        incAr.push(inc[x]);
+        userIncAr[ne[x]] = user[x];
+    }
+    for (let x = 0; x < user.length; x++) {
+        userAr[x] = incAr[ne[x]];
+    }
+}
+allArray();
+console.log(userAr);
+console.log(incAr);
+console.log(ne);
+console.log(userIncAr);
+
 for (let x = 0; x < user.length; x++) {
     userAr.push(user[x]);
 }
