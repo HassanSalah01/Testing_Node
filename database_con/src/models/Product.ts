7b6614d25175cd0261e4e546ba908fb06c208c42
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-03 21:32:32 +0200

    Update_Products_REST

--- database_con/src/models/Product.ts
+++ database_con/src/models/Product.ts
@@ -1,35 +1,56 @@
 import client from "../database";
 
-export type product = {
-    name: string;
+type product = {
     id?: number;
-    age: number;
+    name: string;
+    manufacture: string;
+    amount: number;
 };
 
-export class Product {
+class Product {
     async index() {
         try {
             const connection = await client.connect();
-            const sql = "SELECT * FROM product ";
-            const query = await connection.query(sql);
-            await connection.release();
-            return query.rows;
+            const sql = "SELECT * FROM products";
+            const result = await connection.query(sql);
+            connection.release();
+            return result.rows;
         } catch (error) {
-            throw new Error("No Table Were Found");
+            throw new Error("No Table Were Found ");
         }
     }
-    async create(product: product) {
+
+    async create(p: product) {
         try {
             const connection = await client.connect();
-            const sql = "INSERT INTO product VALUES ($1,$2) Returning *";
-            const querry = await connection.query(sql, [
-                product.name,
-                product.age,
+            const sql =
+                "INSERT INTO products (name ,manufacture,amount) VALUES($1,$2,$3) RETURNING *";
+            const result = await connection.query(sql, [
+                p.name,
+                p.manufacture,
+                p.amount,
             ]);
-            await connection.release();
-            return querry.rows[0];
+            connection.release();
+            return result.rows[0];
         } catch (error) {
-            throw new Error("Coudnt Create Element");
+            throw new Error("Product Is Not Created");
+        }
+    }
+    async update(p: product) {
+        try {
+            const connection = await client.connect();
+            const sql =
+                "UPDATE products set name = $1, munufacture=$2,amount=$3 WHERE id = $4 RETURNING *";
+            const result = await connection.query(sql, [
+                p.name,
+                p.manufacture,
+                p.amount,
+                p.id,
+            ]);
+            connection.release();
+            return result.rows[0];
+        } catch (error) {
+            throw new Error("Product Is Not Created");
         }
     }
     async delete(id:number) {
