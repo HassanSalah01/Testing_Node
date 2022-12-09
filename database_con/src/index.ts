977f2ec178f02d957f7eb5a4205df3bd9df12a15
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-09 23:18:03 +0200

    Updated_index.ts

--- database_con/src/index.ts
+++ database_con/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Application } from "express";
-import client from "./database";
+import cors from "cors";
 import routes from "./routes/index";
 import dotenv from "dotenv";
 const app = express();
@@ -7,8 +7,10 @@ dotenv.config();
 const { SERVER_PORT } = process.env;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
+
+app.use("/", routes);
 
 app.get("/", (req: Request, res: Response) => {
     res.statusCode = 200;
@@ -22,6 +24,22 @@ app.listen(SERVER_PORT, () => {
 //     const connection = await client.connect();
 //     const sql = "select * from products";
 //     const result = await connection.query(sql);
+
+//     const p = {
+//         name: "ii34324ii",
+//         manufacture: "tttt",
+//         amount: 544,
+//         id: 1,
+//     };
+
+//     const sql =
+//         "UPDATE products set product_name=$1 ,product_amount=$2 ,product_manufacture=$3 WHERE id=$4 RETURNING *";
+//     const result = await connection.query(sql, [
+//         p.name,
+//         p.amount,
+//         p.manufacture,
+//         p.id,
+//     ]);
 //     console.log(result.rows);
 //     await connection.release();
 // };
